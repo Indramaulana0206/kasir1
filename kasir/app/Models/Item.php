5da2d720b52fcb9a705da76629e82e63b37746c8
Indramaulana0206 <indralav4@gmail.com>
2024-10-18 19:10:56 +0700

    second

--- kasir/app/Models/Item.php
+++ kasir/app/Models/Item.php
@@ -3,8 +3,20 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Casts\Attribute;
 
 class Item extends Model
 {
-    //
+    use HasFactory;
+    /**
+     * fillable
+     * 
+     * @var array
+     */
+    protected $fillable = [
+        'title',
+        'content',
+    ];
+    
 }
